4914b3d55cca975aeeaa3b96dbe886647301de4b
Mick Sexton <lacrookedbeat@hotmail.com>
2018-04-08 22:32:31 -0700

    refactor quick sort to use while loops in partition function and add recursive calls

--- QuickSort.java
+++ QuickSort.java
@@ -2,57 +2,52 @@
 
 public class QuickSort {
 
-  private static int[] quickSort(int[] arr) {
-    // this function needs to call itself recursively
-    // validators
-    if(arr.length == 1) {
-      return arr;
-    }
+  private static int partition(int[] arr, int left, int right) {
 
-    int low = 0;
-    int high = arr.length - 1;
-    int pivot = arr.length / 2;
+    int i = left, j = right;
+    int temp;
+    int pivotValue = arr[(left + right) / 2];
 
-
-
-    return arr;
-  }
-
-  private static int[] partitionArray(int[] arr) {
-    int low = 0;
-    int high = arr.length - 1;
-
-    int pivot = arr[high];
-    int i = -1;
-
-    // ceiling is - 2 cause pivot takes up the - 1 spot
-    for(int j=low; j <= high - 1; j++) {
-      if (arr[j] <= pivot) {
+    while (i <= j) {
+      while (arr[i] < pivotValue) {
         i++;
-        int temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-      } else {    // if arr[j] > pivot
-        // do nothing
+      }
+      while (arr[j] > pivotValue) {
+        j--;
+      }
+      if (i <= j) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
       }
     }
-    // pivot still in last place, may need swap
-    if (arr[i+1] >= arr[high]) {
-      int temp = arr[high];
-      arr[high] = arr[i+1];
-      arr[i+1] = temp;
-    }
+    return i;
 
-    return arr;
+  }
+
+  private static void quickSort(int[] arr, int left, int right) {
+    // has to be left + right cause these values change recursively
+    int partitionIndex = partition(arr, left, right);
+    if (left < partitionIndex - 1) {
+      quickSort(arr, left, partitionIndex - 1);
+    }
+    if (partitionIndex < right) {
+      quickSort(arr, partitionIndex, right);
+    }
   }
 
   public static void main(String [] args) {
     int[] arr1 = {2, 4, 1, 6, 3, 0, 10, 2};
     int[] arr2 = {10, 80, 30, 90, 40, 50, 70};
-    //arr = quickSort(arr);
+    int[] arr3 = {1, 12, 3, 10, 5, 11, 7};
 
-    arr2 = partitionArray(arr2);
-    for(int number : arr2) {
+    int[] arr = arr1;
+
+    quickSort(arr, 0, arr.length-1);
+
+    for(int number : arr) {
       System.out.print(number + " ");
     }
     System.out.println("");
