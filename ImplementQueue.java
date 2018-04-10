a3edbf9279da5775328f1c7252fde4be389da3aa
Mick Sexton <lacrookedbeat@hotmail.com>
2018-04-09 21:40:27 -0700

    add poll, grow, and shrink functions for array queue

--- ImplementQueue.java
+++ ImplementQueue.java
@@ -8,40 +8,60 @@ import java.util.Scanner;
 public class ImplementQueue {
 
   private static String[] aq;
-  private boolean full = false;
-
-  private static void initializeNewQueue() {
-    for(int i = 0; i < aq.length; i++) {
-      aq[i] = "";
-    }
-  }
+  private static boolean qFull = false;
 
   // add new value to beginning of queue
   private static void offer(String value) {
-    //System.out.println("You entered " + value);
+
+    if(qFull == true) {
+      grow();
+    }
+
     for(int i = aq.length - 1; i >= 0; i--) {
-      if(aq[i].equals("")) {
+      if(aq[i] == null) {
         aq[i] = value;
         System.out.println(aq[i] + " added to index " + i);
+        if (i == 0) {
+          qFull = true;
+        }
         break;
       } else {
         // keep going
       }
-
     }
-
-
   }
 
   // remove value from end of queue
   private static void poll() {
+    int spot = aq.length - 1;
+    if (aq[spot] == null) {
+      System.out.println("No entries to remove in the queue.");
+      return;
+    } else {
+      System.out.println(aq[spot] + " removed from queue.");
+    }
+    aq[spot] = null;
+    spot--;
+
+    // shift each entry up one index until its null or reach zero
+    while (spot >= 0 && aq[spot] != null) {
+      aq[spot+1] = aq[spot];
+      aq[spot] = null;
+      spot--;
+    }
+    System.out.println("Index at polled: " + spot);
+
+    // check if array size should shrink
+    if ((spot > aq.length * 0.666) && aq.length > 9) {
+      shrink();
+    }
 
   }
 
   private static void printQueue() {
     System.out.println("---Queue Contents---");
     for(int i = aq.length - 1; i >= 0; i--) {
-      if (aq[i].equals("")) {
+      if (aq[i] == null) {
         // do nothing
       } else {  // print element's contents
         System.out.print(aq[i] + " ");
@@ -53,17 +73,46 @@ public class ImplementQueue {
   // grow array if limit is reached
   private static void grow() {
 
+    System.out.println("---Queue MAX reached. Growing!---");
+    // create new array
+    String[] newAQ = new String[aq.length * 2];
+    int oldAQpointer = aq.length - 1;
+    int newAQpointer = newAQ.length - 1;
+
+    while (oldAQpointer >= 0 && aq[oldAQpointer] != null) {
+      newAQ[newAQpointer] = aq[oldAQpointer];
+      oldAQpointer--;
+      newAQpointer--;
+    }
+
+    // set original aq to the new array
+    aq = newAQ;
+    // set qFull back to false
+    qFull = false;
   }
 
   // shrink array ??
   private static void shrink() {
+    System.out.println("---Shrinking queue size to free up space---");
+
+    String[] newAQ = new String[aq.length / 2];
+    int newAQpointer = newAQ.length - 1;
+    int oldAQpointer = aq.length - 1;
+
+    while (aq[oldAQpointer] != null) {
+      newAQ[newAQpointer] = aq[oldAQpointer];
+      newAQpointer--;
+      oldAQpointer--;
+    }
+
+    aq = newAQ;
 
   }
 
   public static void main(String[] args) {
 
-    aq = new String[10];
-    initializeNewQueue();
+    aq = new String[5];
+    //initializeNewQueue();
 
     Scanner scanner = new Scanner(System.in);
     System.out.println("Welcome to the Queue Array");
@@ -84,7 +133,7 @@ public class ImplementQueue {
         offer(newEntry);
 
       } else if (choice.equals("2")) {
-        System.out.println(choice + " POLL selected");
+        poll();
       } else if (choice.equals("3")) {
         printQueue();
       } else {
