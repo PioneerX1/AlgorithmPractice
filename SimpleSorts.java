b009fe25a730629dd8259c297d4446eeb9745c70
Mick Sexton <lacrookedbeat@hotmail.com>
2018-04-23 22:24:05 -0700

    add method for Insertion Sort, logs for numbers on comparisons and swaps, and refactored all 3 sort methods to clone the original array instead operating on it.

--- SimpleSorts.java
+++ SimpleSorts.java
@@ -2,55 +2,105 @@ import java.util.*;
 
 public class SimpleSorts {
 
-  private static int[] bubbleSort(int[] arr) {
+  private static void printArray(int[] arr) {
+    // used by all methods including main to print the current order of contents
+    for(int num : arr) {
+      System.out.print(num + " ");
+    }
+    System.out.println("\n");
+  }
+
+  private static void bubbleSort(int[] arr) {
+    // bubbles up the max values to the right (or top) of the array
+
+    // array is an object and actually a pointer, must be cloned
+    int[] a = new int[arr.length];
+    for(int i = 0; i < a.length; i++) {
+      a[i] = arr[i];
+    }
+
     // for logging BigO efficiency
     int swaps = 0;
     int comparisons = 0;
 
-    for (int high = arr.length - 1; high > 0; high--) {
+    for (int high = a.length - 1; high > 0; high--) {
       for (int low = 0; low < high; low++) {
         comparisons++;
-        if (arr[low] > arr[low + 1]) {
+        if (a[low] > a[low + 1]) {
           // swap
-          int temp = arr[low + 1];
-          arr[low + 1] = arr[low];
-          arr[low] = temp;
+          int temp = a[low + 1];
+          a[low + 1] = a[low];
+          a[low] = temp;
           swaps++;
         }
       }
     }
     System.out.println("--- " + comparisons + " comparisons and " + swaps + " swaps made with BubbleSort ---\n");
-    return arr;
+    printArray(a);
   }
 
-  private static int[] selectionSort(int[] arr) {
+  private static void selectionSort(int[] arr) {
     // reads through whole array (n-1) times, each time, finding the minimum and swapping it with the left most pointer, left pointer is incremented.
+
+    // since passed in array is really an object pointer, must clone
+    int[] a = new int[arr.length];
+    for(int i = 0; i < a.length; i++) {
+      a[i] = arr[i];
+    }
 
     int swaps = 0;
     int comparisons = 0;
 
-    for (int left = 0; left < arr.length - 1; left++) {
-      int least = arr[left];
+    for (int left = 0; left < a.length - 1; left++) {
+      int least = a[left];
       int leastIndex = left;
-      for (int right = left + 1; right < arr.length; right++) {
+      for (int right = left + 1; right < a.length; right++) {
         comparisons++;
-        if (arr[right] < least) {
-          least = arr[right];
+        if (a[right] < least) {
+          least = a[right];
           leastIndex = right;
         }
       }
-      if (least < arr[left]) {
+      if (least < a[left]) {
         //swap
-        int temp = arr[left];
-        arr[left] = arr[leastIndex];
-        arr[leastIndex] = temp;
+        int temp = a[left];
+        a[left] = a[leastIndex];
+        a[leastIndex] = temp;
         swaps++;
       }
     }
 
     System.out.println("--- " + comparisons + " comparisons and " + swaps + " swaps made with SelectionSort ---\n");
 
-    return arr;
+    printArray(a);
+  }
+
+  private static void insertionSort(int[] arr) {
+    // expands out a sorted section of array by shifting items up to insert one in
+
+    // since passed in array is really an object pointer, must clone
+    int[] a = new int[arr.length];
+    for(int i = 0; i < a.length; i++) {
+      a[i] = arr[i];
+    }
+
+    int swaps = 0;
+    int comparisons = 0;
+
+    for (int out = 1; out < a.length; out++) {  // start at 1 so you can assign to 0th element if need be
+      int temp = a[out];  // save that value to be moved
+      int in = out;
+      while (in > 0 && a[in - 1] >= temp) {   // don't decrement in past 0, make sure the decremented value is >= to the value you want to move
+        comparisons++;
+        a[in] = a[in -1];   // shift up
+        in--;   // move in left
+      }
+      swaps++;
+      a[in] = temp;   // fill in value where it needs to go
+    }
+
+    System.out.println("--- " + comparisons + " comparisons and " + swaps + " swaps made with InsertionSort ---\n");
+    printArray(a);
   }
 
   public static void main(String[] args) {
@@ -59,18 +109,12 @@ public class SimpleSorts {
     System.out.println("Simple Sorts demonstrates Bubble, Selection, and Insertion sorts with the following int array.\n");
     System.out.println("Before ....");
     // print array before its sorted
-    for(int num : arr) {
-      System.out.print(num + " ");
-    }
-    System.out.println("\n");
+    printArray(arr);
 
-    arr = selectionSort(arr);
-
-    System.out.println("After ....");
-    for(int num : arr) {
-      System.out.print(num + " ");
-    }
-    System.out.println("\n");
+    // call 3 different simple sorts that also print
+    bubbleSort(arr);
+    selectionSort(arr);
+    insertionSort(arr);
 
   }
 }
